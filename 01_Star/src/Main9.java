b11275e00b458d2f9eb83f3252b610423b682c6c
LeeInGyu <Developlee20@gmail.com>
2025-06-17 17:31:30 +0900

    refactor: 별찍기 9번 문제 수학으로 해결

--- 01_Star/src/Main9.java
+++ 01_Star/src/Main9.java
@@ -1,42 +1,17 @@
 public class Main9 {
     public static void main(String[] args) {
-        int left_empty = 0;
-        int star = 5;
-        int right_empty = 0;
+        int N = 5;
 
-        while(star != -1){
-            for(int i=0; i<left_empty; i++){
-                System.out.print(" ");
-            }
-            for(int i=0; i<star; i++){
-                System.out.print("*");
-            }
-            for(int i=0; i<right_empty; i++){
-                System.out.print(" ");
+        for(int x=0; x<N; x++){
+            for(int y=0; y<N; y++){
+                if(y <= x && y >= (-x + 4) || (y >= x && y <= (-x + 4))){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
             }
             System.out.println();
-            left_empty++;
-            star = star - 2;
-            right_empty++;
-        }
-        star = star + 4;
-        left_empty = left_empty - 2;
-        right_empty = right_empty - 2;
-
-        while(star != 7){
-            for(int i=0; i<left_empty; i++){
-                System.out.print(" ");
-            }
-            for(int i=0; i<star; i++){
-                System.out.print("*");
-            }
-            for(int i=0; i<right_empty; i++){
-                System.out.print(" ");
-            }
-            System.out.println();
-            left_empty--;
-            star = star + 2;
-            right_empty--;
         }
     }
 }
