383101d53b780299d191d7526503013bdcb8f8d2
LeeInGyu <Developlee20@gmail.com>
2025-06-18 14:17:21 +0900

    refactor: 별찍기 10번 문제 수학으로 해결

--- 01_Star/src/Main10.java
+++ 01_Star/src/Main10.java
@@ -1,44 +1,17 @@
 public class Main10 {
     public static void main(String[] args) {
         int N = 5;
-        int star = 1;
-        int mid_empty = N-2;
 
-        for(int i=0; i<N/2; i++){
-            for(int j=0; j<star; j++){
-                System.out.print("*");
-            }
-            for(int j=0; j<mid_empty; j++){
-                System.out.print(" ");
-            }
-            for(int j=0; j<star; j++){
-                System.out.print("*");
+        for(int x=0; x<N; x++){
+            for(int y=0; y<N; y++){
+                if(y <= x && y <= (-x + 4) || (y >= x && y >= (-x + 4))){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
             }
             System.out.println();
-            star++;
-            mid_empty = mid_empty - 2;
-        }
-
-        for(int i=0; i<N; i++){
-            System.out.print("*");
-        }
-        System.out.println();
-        star--;
-        mid_empty = mid_empty + 2;
-
-        for(int i=0; i<N/2; i++){
-            for(int j=0; j<star; j++){
-                System.out.print("*");
-            }
-            for(int j=0; j<mid_empty; j++){
-                System.out.print(" ");
-            }
-            for(int j=0; j<star; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-            star--;
-            mid_empty = mid_empty + 2;
         }
     }
 }
