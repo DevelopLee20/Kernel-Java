cdaefaaf2d6cab858762667c84ad6ecd151f5267
LeeInGyu <developLee20@gmail.com>
2025-07-08 18:14:15 +0900

    6장 연습문제 풀이

--- 00_MySelf/src/Student.java
+++ 00_MySelf/src/Student.java
@@ -1,4 +1,4 @@
-// 6-4
+// 6-4, 6-5
 public class Student {
     String name;
     int ban;
@@ -15,6 +15,10 @@ public class Student {
         return Math.round(getTotal() / 3.0 * 10.0) / 10.0;
     }
 
+    public String info(){
+        return this.name + "," + this.ban + "," + this.no + "," + this.kor + "," + this.eng + "," + this.math + "," + this.getTotal() + "," + this.getAverage();
+    }
+
     public static void main(String[] args) {
         Student s = new Student();
         s.name = "홍길동";
@@ -27,5 +31,7 @@ public class Student {
         System.out.println("이름:" + s.name);
         System.out.println("총점:" + s.getTotal());
         System.out.println("평균:" + s.getAverage());
+
+        System.out.println(s.info());
     }
 }
